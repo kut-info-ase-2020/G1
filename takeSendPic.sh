0448b805e8fbf03b19da8a098dcdd6dca6c5dbca
yoshidalab <yoshidalab@kut192-168-0-030.camera.kochi-tech.ac.jp>
2020-07-22 18:05:06 +0900

    take Pic. and send Pic. to ~/Pictures in server

--- takeSendPic.sh
+++ takeSendPic.sh
@@ -6,5 +6,5 @@ while [ "$1" -ne 0 ]
 do
 	sleep $1;
 	fswebcam -r 680x480 ../Pictures/pic.jpg;
-	scp ../Pictures/pic.jpg root@116.198.163.223:~/Pictures/;
+	sshpass -p "aesg12020@#" scp ../Pictures/pic.jpg root@116.198.163.223:~/Pictures/;
 done
