4642e9f19fe3b54c5a32901986a7e5129b328413
yoshidalab <yoshidalab@kut192-168-0-030.camera.kochi-tech.ac.jp>
2020-07-26 17:29:27 +0900

    socket test

--- test_socket_client.py
+++ test_socket_client.py
@@ -1,6 +1,33 @@
 import socket
+import os, sys
 
-HOST = "127.0.0.1"
+HOST = "0.0.0.0"
+PORT = 50001
+
+def main():
+	image_file = sys.argv[1]
+
+	with open(image_file, 'rb') as f:
+    	binary = f.read()
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# connect server
+	s.connect((host, port))
+
+	# send image to server
+	print("Send " + image_file)
+	s.sendall(binary)
+	# disconnect 
+	s.shutdown(1)
+
+	# receive answer from server (buffer size = 4 bite)
+	res = s.recv(4)
+
+	print("Answer is " + str(res))
+
+if __name__ == "main":
+	main()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, 1235))
