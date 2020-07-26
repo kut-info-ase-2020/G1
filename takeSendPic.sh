fd8a920e886a2eab6bdac461e09603aac1cc3a71
yoshidalab <yoshidalab@kut192-168-0-030.camera.kochi-tech.ac.jp>
2020-07-26 18:53:02 +0900

    change file to send image using python

--- takeSendPic.sh
+++ takeSendPic.sh
@@ -5,6 +5,6 @@
 while [ "$1" -ne 0 ] 
 do
 	sleep $1;
-	fswebcam -r 680x480 ../Pictures/pic.jpg;
-	sshpass -p "aesg12020@#" scp ../Pictures/pic.jpg root@116.198.163.223:~/Pictures/;
+	fswebcam -r 680x480 ../Pictures/pic.ong;
+	python test_socket_client.py ../Pictures/pic.png; 
 done
