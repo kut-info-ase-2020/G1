ec74ef8b942ed83897ef3117b6a1f48f6bc6861c
yoshidalab <yoshidalab@kut192-168-0-030.camera.kochi-tech.ac.jp>
2020-07-26 17:43:17 +0900

    socket test

--- test_socket_client.py
+++ test_socket_client.py
@@ -28,10 +28,3 @@ def main():
 
 if __name__ == "main":
 	main()
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, 1235))
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-msg = s.recv(8)
-print(str(msg))
