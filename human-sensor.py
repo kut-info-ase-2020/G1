5bab9688badddde4d6a1ac910c8dc51b31d3a435
新田雄一郎 <nittayuuichirou@Nittamacair.local>
2020-09-15 17:13:21 +0900

    human-sensor.py

---
+++ human-sensor.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import time
+import RPi.GPIO as GPIO
+
+sensor_pin = 16
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor_pin, GPIO.IN)
+
+
+while True:
+    if( GPIO.input(sensor_pin) == 0 ):
+        print ("OFF")
+
+    else:
+        print("ON")
+
+    time.sleep(1)
