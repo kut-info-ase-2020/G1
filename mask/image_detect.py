1080858dd77c65e27337e7b6680b71e0015e75bf
新田雄一郎 <nittayuuichirou@kut192-168-0-125.camera.kochi-tech.ac.jp>
2020-09-23 18:33:19 +0900

    change

--- mask/image_detect.py
+++ mask/image_detect.py
@@ -14,8 +14,8 @@ import cv2
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_file_path", type=str, default="testing/input/images", help="path to images directory")
-    parser.add_argument("--output_path", type=str, default="testing/output/images", help="output image directory")
+    parser.add_argument("--input_file_path", type=str, default="Picture/", help="path to images directory")
+    parser.add_argument("--output_path", type=str, default="Picture/", help="output image directory")
     parser.add_argument("--model_def", type=str, default="config/yolov3_mask.cfg", help="path to model definition file")
     parser.add_argument("--weights_path", type=str, default="checkpoints/yolov3_ckpt_35.pth", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/mask_dataset.names", help="path to class label file")
