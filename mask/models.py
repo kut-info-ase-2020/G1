0eb1cd2d02be58e8db7df08ea826ee7d1c9cb8e5
Eden KUT <44993998+Edenkut@users.noreply.github.com>
2020-09-29 17:05:01 +0900

    Update models.py

--- mask/models.py
+++ mask/models.py
@@ -5,8 +5,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-from mask.utils.parse_config import parse_model_config
-from mask.utils.utils import build_targets, to_cpu
+from parse_config import parse_model_config
+from utils import build_targets, to_cpu
 
 
 def create_modules(module_defs):
