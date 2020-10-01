5234d3fd42e7b83dd695701ad3d075ecc7e5d26f
yuichi1614 <32477122+yuichi1614@users.noreply.github.com>
2020-10-02 00:05:52 +0900

    Update human-sensor.py

--- human-sensor.py
+++ human-sensor.py
@@ -18,7 +18,7 @@ while True:
 
     #def call_shell():
     	path = os.path.dirname(os.path.abspath(__file__))
-    	cmd = '{}/takeSendPic.sh {}'.format(path, 5)
+    	cmd = '{}/takeSendPic.sh {}'.format(path, 20)
     	res = subprocess.call(cmd, shell=True)
     	print(res)
 
