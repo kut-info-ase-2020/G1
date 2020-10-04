4e0f191f89ffd0814f7b025f6f972d6cb63ece58
新田雄一郎 <245133m@gs.kochi-tech.ac.jp>
2020-10-05 03:12:41 +0900

    maybe finish2

--- socket_host.py
+++ socket_host.py
@@ -62,7 +62,7 @@ def recv_client_data(clientsock):
 	# output is Int (estimate 0 ~ 15)
 	# pleese make and use mathod
 	res = recognition()
-	printn(bin(res))
+	print(bin(res))
 	# send result to client
 	clientsock.sendall(res.to_bytes(1, "big"))
 
