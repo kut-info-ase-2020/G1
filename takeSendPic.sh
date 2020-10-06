c589bbb97842da3e26262a3ecfa4b7b279d8f6a0
CookieTKHS <yoshidalab@kut192-168-0-030.camera.kochi-tech.ac.jp>
2020-10-06 19:31:26 +0900

    tabun owari

--- takeSendPic.sh
+++ takeSendPic.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-# time scale = $1 (arg1)
-# take picture by $1
-# save in ../Pictures
-
-#while [ "$1" -ne 0 ] 
-#do
-	fswebcam -r 680x480 ../Pictures/pic.png;
-	#python showStat.py ../Pictures/pic.png; 
-	#sleep $1;
-#done
+fswebcam -r 640x480 ../Pictures/pic.png;
